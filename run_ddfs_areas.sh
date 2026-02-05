1366722f61a7f31652f232c2c44246c67d60c65e
Sam Philipsborn <sp624@artemis-a40-11.local>
2026-02-05 16:35:02 +0000

    adding runs of ddfs, and sh scripts

---
+++ run_ddfs_areas.sh
@@ -0,0 +1,12 @@
+python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/WD01_randoms.parquet --region WD01 > waves_wd01_area.txt
+python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/WD02_randoms.parquet --region WD02 > waves_wd02_area.txt
+python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/WD03_randoms.parquet --region WD03 > waves_wd03_area.txt
+python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/WD10_randoms.parquet --region WD10 > waves_wd10_area.txt
+
+
+#echo "SECOND BATCH"
+
+#python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/second_batch/waves-wide_n_randoms.parquet --region waves-wide_n > waves_n_area_2.txt
+#python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/second_batch/waves-wide_s_randoms.parquet --region waves-wide_s > waves_s_area_2.txt
+#python3 find_areas.py /mnt/lustre/projects/astro/general/sp624/waves_randoms/second_batch/waves-deep_randoms.parquet --region waves-deep > waves_deep_area_2.txt
+
